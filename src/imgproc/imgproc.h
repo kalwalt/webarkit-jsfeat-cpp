de79a89e5fb3a2262d442a7d516de624c1c7f328
kalwalt <github@kalwaltart.it>
2022-08-08 16:39:28 +0200

    another grayscale test

--- src/imgproc/imgproc.h
+++ src/imgproc/imgproc.h
@@ -1,9 +1,9 @@
 #ifndef IMGPROC_H
 #define IMGPROC_H
 
+#include <iostream>
 #include <matrix_t/matrix_t.h>
 #include <types/types.h>
-#include <iostream>
 
 namespace jsfeat {
 class imgproc {
@@ -14,6 +14,7 @@ public:
     if (!code) {
       code = Colors::COLOR_RGBA2GRAY;
     }
+    int videosize = w * h;
     int x = 0;
     int y = 0;
     int i = 0;
@@ -43,32 +44,39 @@ public:
       std::cout << "vector is empty" << std::endl;
     }
     // this should print a zero value
-    std::cout << "value: " << (int)src->u8.at(0) <<std::endl;
+    std::cout << "value: " << (int)src->u8.at(0) << std::endl;
 
-    for (y = 0; y < h; ++y, j += w, i += w * cn) {
+    /*for (y = 0; y < h; ++y, j += w, i += w * cn) {
       // probably we can do this in javascript but not in C++
-      /*for (x = 0, ir = i, jr = j; x <= w - 4; x += 4, ir += cn << 2, jr += 4) {
-        dst->u8.at(jr) = (u_char)(src->u8.at(ir) * coeff_r + src->u8.at(ir + 1) * coeff_g +
-                      src->u8.at(ir + 2) * coeff_b + 8192) >>
-                     14;
-        dst->u8.at(jr + 1) =
-            (u_char)(src->u8.at(ir + cn) * coeff_r + src->u8.at(ir + cn + 1) * coeff_g +
-             src->u8.at(ir + cn + 2) * coeff_b + 8192) >>
-            14;
-        dst->u8.at(jr + 2) =
-            (u_char)(src->u8.at(ir + cn2) * coeff_r + src->u8.at(ir + cn2 + 1) * coeff_g +
-             src->u8.at(ir + cn2 + 2) * coeff_b + 8192) >>
-            14;
-        dst->u8.at(jr + 3) =
-            (u_char)(src->u8.at(ir + cn3) * coeff_r + src->u8.at(ir + cn3 + 1) * coeff_g +
-             src->u8.at(ir + cn3 + 2) * coeff_b + 8192) >>
-            14;
-      }*/
-      for (; x < w; ++x, ++jr, ir += cn) {
-        dst->u8.at(jr) = (u_char)(src->u8.at(ir) * coeff_r + src->u8.at(ir + 1) * coeff_g +
-                      src->u8.at(ir + 2) * coeff_b + 8192) >>
-                     14;
+      for (x = 0, ir = i, jr = j; x <= w - 4; x += 4, ir += cn << 2, jr += 4) {
+        dst->u8.at(jr) = (u_char)(src->u8.at(ir) * coeff_r + src->u8.at(ir + 1)
+    * coeff_g + src->u8.at(ir + 2) * coeff_b + 8192) >> 14; dst->u8.at(jr + 1) =
+            (u_char)(src->u8.at(ir + cn) * coeff_r + src->u8.at(ir + cn + 1) *
+    coeff_g + src->u8.at(ir + cn + 2) * coeff_b + 8192) >> 14; dst->u8.at(jr +
+    2) = (u_char)(src->u8.at(ir + cn2) * coeff_r + src->u8.at(ir + cn2 + 1) *
+    coeff_g + src->u8.at(ir + cn2 + 2) * coeff_b + 8192) >> 14; dst->u8.at(jr +
+    3) = (u_char)(src->u8.at(ir + cn3) * coeff_r + src->u8.at(ir + cn3 + 1) *
+    coeff_g + src->u8.at(ir + cn3 + 2) * coeff_b + 8192) >> 14;
       }
+      for (; x < w; ++x, ++jr, ir += cn) {
+        dst->u8.at(jr) = (u_char)(src->u8.at(ir) * coeff_r + src->u8.at(ir + 1)
+    * coeff_g + src->u8.at(ir + 2) * coeff_b + 8192) >> 14;
+      }
+    }*/
+    // code from jsartoolkit5
+    int q = 0;
+    int r;
+    int g;
+    int b;
+
+    for (int p = 0; p < videosize-1; p++) {
+      r = src->u8.at(q + 0), g = src->u8.at(q + 1), b = src->u8.at(q + 2);
+      // https://stackoverflow.com/a/596241/5843642
+      dst->u8.at(p) = (r + r + r + b + g + g + g + g) >> 3;
+      q += 4;
+    }
+    for (int i = 0; i < dst->u8.size(); i++) {
+      std::cout << "value dst: " << (int)dst->u8.at(i) << std::endl;
     }
   };
 };
