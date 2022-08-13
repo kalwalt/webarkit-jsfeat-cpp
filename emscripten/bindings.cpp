08c77c5a4bb80ca80ce54a3de9a94296ddf8a0e7
kalwalt <github@kalwaltart.it>
2022-08-13 15:56:36 +0200

    adding more Types

--- emscripten/bindings.cpp
+++ emscripten/bindings.cpp
@@ -9,6 +9,23 @@ EMSCRIPTEN_BINDINGS(webarkit) {
     .constructor<int, int, int, int>()
     .function("allocate", &matrix_t::allocate)
     .function("resize", &matrix_t::resize);
+
+    enum_<Types>("Types")
+    .value("U8_t", U8_t)
+    .value("S32_t", S32_t)
+    .value("F32_t", F32_t)
+    .value("S64_t", S64_t)
+    .value("F64_t", F64_t)
+    .value("C1_t", C1_t)
+    .value("C2_t", C2_t)
+    .value("C3_t", C3_t)
+    .value("C4_t", C4_t);
+
+    enum_<Colors>("Colors")
+    .value("COLOR_RGBA2GRAY", Colors::COLOR_RGBA2GRAY)
+    .value("COLOR_RGB2GRAY", COLOR_RGB2GRAY)
+    .value("COLOR_BGRA2GRAY", COLOR_BGRA2GRAY)
+    .value("COLOR_BGR2GRAY", COLOR_BGR2GRAY);
+
     function("toGrayscale", &toGrayscale, allow_raw_pointers());
-    function("hello", &hello);
 }
