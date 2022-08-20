24d053892838fea4aa41111d5188e6d3de8f224b
kalwalt <github@kalwaltart.it>
2022-08-20 16:22:45 +0200

    Grayscale_s seems to works correctly

--- src/jsfeat.cpp
+++ src/jsfeat.cpp
@@ -7,3 +7,8 @@ void grayscale(u_char *src, int w, int h, matrix_t *dst, int code) {
     imgproc img;
     img.grayscale(src, w, h, dst, code);
 };
+
+void grayscale_s(u_char *src, int w, int h, Mat_t dst, int code) { 
+    imgproc img;
+    img.grayscale_s(src, w, h, dst, code);
+};
