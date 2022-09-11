7a9a68d2a509e18b2f32ad1e29f70efdfa05abf3
kalwalt <github@kalwaltart.it>
2022-09-11 22:51:54 +0200

    new imgproc export with embind

--- src/imgproc/imgproc.h
+++ src/imgproc/imgproc.h
@@ -3,14 +3,16 @@
 
 #include <matrix_t/matrix_t.h>
 #include <types/types.h>
-#include <vector>
+//#include <vector>
 #include <jslog/jslog.h>
 
 namespace jsfeat {
 
 class imgproc {
 public:
-  void grayscale_m(matrix_t *src, int w, int h, matrix_t *dst, int code) {
+  void grayscale_m(uintptr_t src, int w, int h, uintptr_t dst, int code) {
+    auto ptrSrc = reinterpret_cast<matrix_t*>(src);
+    auto ptrDst = reinterpret_cast<matrix_t*>(dst);
     // this is default image data representation in browser
     if (!code) {
       code = Colors::COLOR_RGBA2GRAY;
@@ -37,45 +39,47 @@ public:
     int cn2 = cn << 1;
     int cn3 = (cn * 3) | 0;
 
-    dst->resize(w, h, 1);
+    ptrDst->resize(w, h, 1);
 
-    if (src->dt->u8.empty()) {
+    if (ptrSrc->dt->u8.empty()) {
       JSLOGe("vector is empty");
     }
 
     for (y = 0; y < h; ++y, j += w, i += w * cn) {
       // probably we can do this in javascript but not in C++
       for (x = 0, ir = i, jr = j; x <= w - 4; x += 4, ir += cn << 2, jr += 4) {
-        dst->dt->u8.at(jr) =
-            (src->dt->u8.at(ir) * coeff_r + src->dt->u8.at(ir + 1) * coeff_g +
-             src->dt->u8.at(ir + 2) * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr) =
+            (ptrSrc->dt->u8.at(ir) * coeff_r + ptrSrc->dt->u8.at(ir + 1) * coeff_g +
+             ptrSrc->dt->u8.at(ir + 2) * coeff_b + 8192) >>
             14;
-        dst->dt->u8.at(jr + 1) =
-            (src->dt->u8.at(ir + cn) * coeff_r +
-             src->dt->u8.at(ir + cn + 1) * coeff_g +
-             src->dt->u8.at(ir + cn + 2) * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr + 1) =
+            (ptrSrc->dt->u8.at(ir + cn) * coeff_r +
+             ptrSrc->dt->u8.at(ir + cn + 1) * coeff_g +
+             ptrSrc->dt->u8.at(ir + cn + 2) * coeff_b + 8192) >>
             14;
-        dst->dt->u8.at(jr + 2) =
-            (src->dt->u8.at(ir + cn2) * coeff_r +
-             src->dt->u8.at(ir + cn2 + 1) * coeff_g +
-             src->dt->u8.at(ir + cn2 + 2) * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr + 2) =
+            (ptrSrc->dt->u8.at(ir + cn2) * coeff_r +
+             ptrSrc->dt->u8.at(ir + cn2 + 1) * coeff_g +
+             ptrSrc->dt->u8.at(ir + cn2 + 2) * coeff_b + 8192) >>
             14;
-        dst->dt->u8.at(jr + 3) =
-            (src->dt->u8.at(ir + cn3) * coeff_r +
-             src->dt->u8.at(ir + cn3 + 1) * coeff_g +
-             src->dt->u8.at(ir + cn3 + 2) * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr + 3) =
+            (ptrSrc->dt->u8.at(ir + cn3) * coeff_r +
+             ptrSrc->dt->u8.at(ir + cn3 + 1) * coeff_g +
+             ptrSrc->dt->u8.at(ir + cn3 + 2) * coeff_b + 8192) >>
             14;
       }
       for (; x < w; ++x, ++jr, ir += cn) {
-        dst->dt->u8.at(jr) =
-            (src->dt->u8.at(ir) * coeff_r + src->dt->u8.at(ir + 1) * coeff_g +
-             src->dt->u8.at(ir + 2) * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr) =
+            (ptrSrc->dt->u8.at(ir) * coeff_r + ptrSrc->dt->u8.at(ir + 1) * coeff_g +
+             ptrSrc->dt->u8.at(ir + 2) * coeff_b + 8192) >>
             14;
       }
     }
   };
 
-  void grayscale(u_char *src, int w, int h, matrix_t *dst, int code) {
+  void grayscale(uintptr_t src, int w, int h, uintptr_t dst, int code) {
+    auto ptrSrc = reinterpret_cast<u_char*>(src);
+    auto ptrDst = reinterpret_cast<matrix_t*>(dst);
     // this is default image data representation in browser
     if (!code) {
       code = Colors::COLOR_RGBA2GRAY;
@@ -102,34 +106,34 @@ public:
     int cn2 = cn << 1;
     int cn3 = (cn * 3) | 0;
 
-    dst->resize(w, h, 1);
+    ptrDst->resize(w, h, 1);
 
     for (y = 0; y < h; ++y, j += w, i += w * cn) {
       for (x = 0, ir = i, jr = j; x <= w - 4; x += 4, ir += cn << 2, jr += 4) {
-        dst->dt->u8.at(jr) =
-            (src[ir] * coeff_r + src[ir + 1] * coeff_g +
-             src[ir + 2] * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr) =
+            (ptrSrc[ir] * coeff_r + ptrSrc[ir + 1] * coeff_g +
+             ptrSrc[ir + 2] * coeff_b + 8192) >>
             14;
-        dst->dt->u8.at(jr + 1) =
-            (src[ir + cn] * coeff_r +
-             src[ir + cn + 1] * coeff_g +
-             src[ir + cn + 2] * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr + 1) =
+            (ptrSrc[ir + cn] * coeff_r +
+             ptrSrc[ir + cn + 1] * coeff_g +
+             ptrSrc[ir + cn + 2] * coeff_b + 8192) >>
             14;
-        dst->dt->u8.at(jr + 2) =
-            (src[ir + cn2] * coeff_r +
-             src[ir + cn2 + 1] * coeff_g +
-             src[ir + cn2 + 2] * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr + 2) =
+            (ptrSrc[ir + cn2] * coeff_r +
+             ptrSrc[ir + cn2 + 1] * coeff_g +
+             ptrSrc[ir + cn2 + 2] * coeff_b + 8192) >>
             14;
-        dst->dt->u8.at(jr + 3) =
-            (src[ir + cn3] * coeff_r +
-             src[ir + cn3 + 1] * coeff_g +
-             src[ir + cn3 + 2] * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr + 3) =
+            (ptrSrc[ir + cn3] * coeff_r +
+             ptrSrc[ir + cn3 + 1] * coeff_g +
+             ptrSrc[ir + cn3 + 2] * coeff_b + 8192) >>
             14;
       }
       for (; x < w; ++x, ++jr, ir += cn) {
-        dst->dt->u8.at(jr) =
-            (src[ir] * coeff_r + src[ir + 1] * coeff_g +
-             src[ir + 2] * coeff_b + 8192) >>
+        ptrDst->dt->u8.at(jr) =
+            (ptrSrc[ir] * coeff_r + ptrSrc[ir + 1] * coeff_g +
+             ptrSrc[ir + 2] * coeff_b + 8192) >>
             14;
       }
     }
