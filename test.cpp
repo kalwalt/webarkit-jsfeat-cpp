d15dbcaf75ac7f98b8ac7c4b82918eef38c87f0e
kalwalt <github@kalwaltart.it>
2022-09-13 21:33:29 +0200

    cleaning the code - rectify_patch included in orb.h

--- test.cpp
+++ test.cpp
@@ -1,5 +1,4 @@
 #include <matrix_t/matrix_t.h>
-#include <orb/rectify_patch.h>
 #include <types/types.h>
 #include <imgproc/imgproc.h>
 #include <stdlib.h>
@@ -7,15 +6,6 @@
 using namespace jsfeat;
 
 int main() {
-  srand(1);
-  for (int i = 0; i < 4; ++i) {
-    float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-    // cout << r << endl;
-    rectify_patch(r, 2, 3, 4);
-  }
-  test_matrix_t();
-  matrix_t t(3, 1, 0x0100 | 0x01, 0x0100);
-  test2_matrix_t(t);
   matrix_t *src = new matrix_t(2, 2, 0x0100 | 0x04, 0x0100);
   matrix_t *dst = new matrix_t(2, 2, 0x0100 | 0x01, 0x0100);
   src->allocate();
